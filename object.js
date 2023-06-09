6ba46faeb12686dd1ba35e90ecd6edd1a3657aed
Naim Hasan <naimarafat7@gmail.com>
2023-06-09 11:52:59 -0700

    object

---
+++ object.js
@@ -0,0 +1,29 @@
+//create Object Using object Literals
+
+const player = {};
+player.name = 'Naim Hasan';//properties
+player.specialty = 'Programmer';//properties
+
+//Method
+player.bat = function () {
+    console.log('Swing your bat');
+}
+
+// console.log(player);
+// player.bat();
+
+
+//most improtant
+//Object literal syntax
+
+const parson = {
+    name: 'Naim',
+    age: 20,
+    job: 'software eng'
+};
+
+console.log(parson);
+
+
+
+
